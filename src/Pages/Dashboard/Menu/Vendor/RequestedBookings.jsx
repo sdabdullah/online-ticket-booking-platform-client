a968e6b8fdc4f1bfb90cb5cd0802347b3e54184a
sdabdullah <ab.faishal@gmail.com>
2025-12-16 01:06:05 +0600

    added Revenue overview charts

--- src/Pages/Dashboard/Menu/Vendor/RequestedBookings.jsx
+++ src/Pages/Dashboard/Menu/Vendor/RequestedBookings.jsx
@@ -8,50 +8,50 @@ const RequestedBookings = () => {
                 <Table hoverable>
                     <TableHead>
                         <TableRow>
-                            <TableHeadCell>Ticket Title</TableHeadCell>
-                            <TableHeadCell>Amount</TableHeadCell>
-                            <TableHeadCell>Payment Date</TableHeadCell>
-                            <TableHeadCell>Transaction ID</TableHeadCell>
+                            <TableHeadCell>User name/emai</TableHeadCell>
+                            <TableHeadCell>Ticket title</TableHeadCell>
+                            <TableHeadCell>Booking Quantity</TableHeadCell>
+                            <TableHeadCell>Total price (unit price * Booking Quantity) </TableHeadCell>
                             <TableHeadCell>
-                                <span className="sr-only">Edit</span>
+                                <span className="sr-only">Accept and Reject buttons</span>
                             </TableHeadCell>
                         </TableRow>
                     </TableHead>
                     <TableBody className="divide-y">
                         <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
                             <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                Bus Ticket
+                                Abdullah
                             </TableCell>
-                            <TableCell>250</TableCell>
-                            <TableCell>20/12/25</TableCell>
-                            <TableCell>TXN3872XG9</TableCell>
+                            <TableCell>But Ticket</TableCell>
+                            <TableCell>10</TableCell>
+                            <TableCell>500</TableCell>
                             <TableCell>
                                 <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                                    Edit
+                                    Accept/Reject
                                 </a>
                             </TableCell>
                         </TableRow>
                         <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
                             <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                Bus Ticket
+                                Faishal
                             </TableCell>
-                            <TableCell>250</TableCell>
-                            <TableCell>20/12/25</TableCell>
-                            <TableCell>TXN3872XG9</TableCell>
+                            <TableCell>But Ticket</TableCell>
+                            <TableCell>20</TableCell>
+                            <TableCell>300</TableCell>
                             <TableCell>
                                 <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                                    Edit
+                                    Accept/Reject
                                 </a>
                             </TableCell>
                         </TableRow>
                         <TableRow className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Bus Ticket</TableCell>
+                            <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Ahmad</TableCell>
                             <TableCell>250</TableCell>
                             <TableCell>20/12/25</TableCell>
                             <TableCell>TXN3872XG9</TableCell>
                             <TableCell>
                                 <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-                                    Edit
+                                    Accept/Reject   
                                 </a>
                             </TableCell>
                         </TableRow>
--- src/Pages/Dashboard/Menu/Vendor/RevenueOverview.jsx
+++ src/Pages/Dashboard/Menu/Vendor/RevenueOverview.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { dataLine, dataBar } from './../../chartData'
+import { Bar, Line } from 'react-chartjs-2';
 
 const RevenueOverview = () => {
     return (
-        <div>
-            RevenueOverview
+        <div className='p-8'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                <div className='bg-white p-4 dark:bg-gray-800 rounded-lg shadow-md'>
+                    <h3 className='text-lg font-semibold mb-4'>Sales Data</h3>
+                    <Line data={dataLine} />
+                </div>
+                <div className='bg-white p-4 dark:bg-gray-800 rounded-lg shadow-md'>
+                    <h3 className='text-lg font-semibold mb-4'>Products Data</h3>
+                    <Bar data={dataBar} />
+                </div>
+            </div>
         </div>
     );
 };
